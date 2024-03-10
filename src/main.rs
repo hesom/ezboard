22c423e7a98476a9b125e2dce37de492f2a5f175
hesom <sommerhoff.hendrik@gmail.com>
2024-03-10 01:34:43 +0100

    Implement polymorphic input handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
-use ezboard::add;
+use std::{fs::File, io::{BufRead, BufReader, stdin}, path::PathBuf};
+use clap::Parser;
+use anyhow::{Result, Context};
 
-fn main() {
-    let a = 1;
-    let b = 2;
-    println!("Sum is: {}", add(a, b));
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    path: Option<PathBuf>,
+}
+
+fn get_input_stream(Cli {path, ..}: &Cli) -> Result<Box<dyn BufRead>>{
+    let reader: Box<dyn BufRead> = match path {
+        Some(path) => {
+            path.is_file()
+                .then(|| 
+                      Box::new(BufReader::new(File::open(path)
+                                              .context("File exists, but still error reading file").unwrap()))
+                     )
+                .context("Path is not a file")?
+        },
+        None => Box::new(BufReader::new(stdin())),
+    };
+
+    Ok(reader)
+}
+
+fn main() ->  Result <()> {
+    let args = Cli::parse();
+    
+    let reader = get_input_stream(&args)?;
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
