84cbe019834c9b7191251ecd9fdf15552b1ec05a
hesom <sommerhoff.hendrik@gmail.com>
2024-03-10 01:40:07 +0100

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::{fs::File, io::{BufRead, BufReader, stdin}, path::PathBuf};
+use anyhow::{Context, Result};
 use clap::Parser;
-use anyhow::{Result, Context};
+use std::{
+    fs::File,
+    io::{stdin, BufRead, BufReader},
+    path::PathBuf,
+};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -8,25 +12,27 @@ struct Cli {
     path: Option<PathBuf>,
 }
 
-fn get_input_stream(Cli {path, ..}: &Cli) -> Result<Box<dyn BufRead>>{
+fn get_input_stream(Cli { path, .. }: &Cli) -> Result<Box<dyn BufRead>> {
     let reader: Box<dyn BufRead> = match path {
-        Some(path) => {
-            path.is_file()
-                .then(|| 
-                      Box::new(BufReader::new(File::open(path)
-                                              .context("File exists, but still error reading file").unwrap()))
-                     )
-                .context("Path is not a file")?
-        },
+        Some(path) => path
+            .is_file()
+            .then(|| {
+                Box::new(BufReader::new(
+                    File::open(path)
+                        .context("File exists, but still error reading file")
+                        .unwrap(),
+                ))
+            })
+            .context("Path is not a file")?,
         None => Box::new(BufReader::new(stdin())),
     };
 
     Ok(reader)
 }
 
-fn main() ->  Result <()> {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    
+
     let reader = get_input_stream(&args)?;
 
     for line in reader.lines() {
